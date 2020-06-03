f1391310bb90109a940126c7d1cae41b228c860e
aniquequreshi <anique.qureshi@gmail.com>
2020-06-03 02:50:25 -0400

    Import Export of Users to Admin

--- questions/resources.py
+++ questions/resources.py
@@ -1,5 +1,6 @@
 from import_export import resources
-from .models import Choice, ChoiceGroup, Question
+from .models import Choice, ChoiceGroup, Question, Subject
+from django.contrib.auth.models import User
 
 class ChoiceResource(resources.ModelResource):
     class Meta:
@@ -12,3 +13,12 @@ class ChoiceGroupResource(resources.ModelResource):
 class QuestionResource(resources.ModelResource):
     class Meta:
         model = Question
+
+class SubjectResource(resources.ModelResource):
+    class Meta:
+        model = Subject
+
+class UserResource(resources.ModelResource):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
